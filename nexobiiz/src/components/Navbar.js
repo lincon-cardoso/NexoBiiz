7daa5dd30dc2da73221ef59d771374b8c3ee3afe
Lincon Cardoso <59518299+lincon-cardoso@users.noreply.github.com>
2024-09-26 01:40:10 -0300

    ahtesteteh

--- nexobiiz/src/components/Navbar.js
+++ nexobiiz/src/components/Navbar.js
@@ -12,7 +12,7 @@ function Navbar() {
         <span>NexoBiiz</span>
       </div>
       <ul className="navbar__menu">
-        <li>Produtossdafaafwew</li>
+        <li>Produtos</li>
         <li>Planos</li>
         <li>Suporte</li>
       </ul>
