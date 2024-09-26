e7dc3af367dfd135c019b9beb3024b67919bad68
Lincon Cardoso <59518299+lincon-cardoso@users.noreply.github.com>
2024-09-26 00:03:43 -0300

    inicio projeto.

---
+++ nexobiiz/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+// import "./Navbar.scss"; // Importar o arquivo de estilos
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar__logo">NexoBiiz</div>
+      <img src="/assets/img/logo.png" alt="logo" />
+    </nav>
+  );
+}
+
+export default Navbar;
