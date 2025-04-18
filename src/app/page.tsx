2c64a71231a6249781cf378ea6bb6ba53b44594b
Lincon <linkon789@gmail.com>
2025-04-18 13:13:20 -0300

    feat: corrige importação do componente Home para padrão ES6

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { Cabecalho } from '@/components/cabecalho/Cabecalho';
 import { Rodape } from '@/components/rodape/Rodape';
-import { Home } from '@/app/home/page';
+import Home from '@/app/home/page';
 
 export default function HomePageComponent() {
   return (
