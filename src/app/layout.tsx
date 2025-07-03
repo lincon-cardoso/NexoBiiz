1b408316b9e25801abefe2d1f0f6e0ba42b829ff
Lincon <linkon789@gmail.com>
2025-07-02 22:04:31 -0300

    feat: ajusta formatação e estrutura da página de registro e adiciona nova página de Dashboard

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,13 @@
 import type { Metadata } from "next";
 import "@/style/global.scss";
 
-
-
 export const metadata: Metadata = {
   title: "Nexobiiz - Transformando Ideias em Soluções",
-  description: "Nexobiiz é uma plataforma inovadora focada em criar soluções digitais personalizadas para o seu negócio.",
+  description:
+    "Nexobiiz é uma plataforma inovadora focada em criar soluções digitais personalizadas para o seu negócio.",
 };
 
-export const dynamic = 'force-dynamic';
+export const dynamic = "force-dynamic";
 
 export default function RootLayout({
   children,
@@ -17,9 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR" suppressHydrationWarning>
-      <body >
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 import { useState } from "react";
 import styles from "@/style/register/register.module.scss";
 import Link from "next/link";
@@ -75,128 +75,134 @@ export default function RegisterPage() {
 
   return (
     <main className={styles.registerPage}>
-      <div className={styles.registerContainer}>
-        <h1 className={styles.registerTitle}>Register</h1>
-        <form className={styles.registerForm} onSubmit={handleSubmit}>
-          <div className={styles.formGroup}>
-            <label htmlFor="name" className={styles.formLabel}>
-              Nome
-            </label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              value={formData.name}
-              onChange={handleChange}
-              className={styles.formInput}
-              placeholder="Digite seu nome"
-            />
-            {errors.name && <span className={styles.error}>{errors.name}</span>}
-          </div>
-          <div className={styles.formGroup}>
-            <label htmlFor="username" className={styles.formLabel}>
-              Username
-            </label>
-            <input
-              type="text"
-              id="username"
-              name="username"
-              value={formData.username}
-              onChange={handleChange}
-              className={styles.formInput}
-              placeholder="Digite seu username"
-            />
-            {errors.username && (
-              <span className={styles.error}>{errors.username}</span>
-            )}
-          </div>
-          <div className={styles.formGroup}>
-            <label htmlFor="email" className={styles.formLabel}>
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              className={styles.formInput}
-              placeholder="Digite seu email"
-            />
-            {errors.email && (
-              <span className={styles.error}>{errors.email}</span>
-            )}
-          </div>
-          <div className={styles.formGroup}>
-            <label htmlFor="password" className={styles.formLabel}>
-              Senha
-            </label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              value={formData.password}
-              onChange={handleChange}
-              className={styles.formInput}
-              placeholder="Digite sua senha"
-            />
-            {errors.password && (
-              <span className={styles.error}>{errors.password}</span>
-            )}
-          </div>
-          <div className={styles.formGroup}>
-            <label htmlFor="company" className={styles.formLabel}>
-              Nome da Empresa
-            </label>
-            <input
-              type="text"
-              id="company"
-              name="company"
-              value={formData.company}
-              onChange={handleChange}
-              className={styles.formInput}
-              placeholder="Digite o nome da empresa"
-            />
-          </div>
-          <div className={styles.formGroup}>
-            <label htmlFor="cnpj" className={styles.formLabel}>
-              CNPJ
-            </label>
-            <input
-              type="text"
-              id="cnpj"
-              name="cnpj"
-              value={formData.cnpj}
-              onChange={handleChange}
-              className={styles.formInput}
-              placeholder="Digite o CNPJ"
-            />
-            {errors.cnpj && <span className={styles.error}>{errors.cnpj}</span>}
-          </div>
-          <div className={styles.formGroup}>
-            <label htmlFor="phone" className={styles.formLabel}>
-              Telefone
-            </label>
-            <input
-              type="tel"
-              id="phone"
-              name="phone"
-              value={formData.phone}
-              onChange={handleChange}
-              className={styles.formInput}
-              placeholder="Digite seu telefone"
-            />
-            {errors.phone && (
-              <span className={styles.error}>{errors.phone}</span>
-            )}
-          </div>
-          <button type="submit" className={styles.formButton}>
-            Register
-          </button>
-          <button type="button" className={styles.formButton}>
-            <Link href="/">Voltar</Link>
-          </button>
-        </form>
+      <div className={styles.registerPage}>
+        <div className={styles.registerContainer}>
+          <h1 className={`${styles.registerTitle} fade-in-out`}>Register</h1>
+          <form className={styles.registerForm} onSubmit={handleSubmit}>
+            <div className={styles.formGroup}>
+              <label htmlFor="name" className={styles.formLabel}>
+                Nome
+              </label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+                className={styles.formInput}
+                placeholder="Digite seu nome"
+              />
+              {errors.name && (
+                <span className={styles.error}>{errors.name}</span>
+              )}
+            </div>
+            <div className={styles.formGroup}>
+              <label htmlFor="username" className={styles.formLabel}>
+                Username
+              </label>
+              <input
+                type="text"
+                id="username"
+                name="username"
+                value={formData.username}
+                onChange={handleChange}
+                className={styles.formInput}
+                placeholder="Digite seu username"
+              />
+              {errors.username && (
+                <span className={styles.error}>{errors.username}</span>
+              )}
+            </div>
+            <div className={styles.formGroup}>
+              <label htmlFor="email" className={styles.formLabel}>
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                className={styles.formInput}
+                placeholder="Digite seu email"
+              />
+              {errors.email && (
+                <span className={styles.error}>{errors.email}</span>
+              )}
+            </div>
+            <div className={styles.formGroup}>
+              <label htmlFor="password" className={styles.formLabel}>
+                Senha
+              </label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+                className={styles.formInput}
+                placeholder="Digite sua senha"
+              />
+              {errors.password && (
+                <span className={styles.error}>{errors.password}</span>
+              )}
+            </div>
+            <div className={styles.formGroup}>
+              <label htmlFor="company" className={styles.formLabel}>
+                Nome da Empresa
+              </label>
+              <input
+                type="text"
+                id="company"
+                name="company"
+                value={formData.company}
+                onChange={handleChange}
+                className={styles.formInput}
+                placeholder="Digite o nome da empresa"
+              />
+            </div>
+            <div className={styles.formGroup}>
+              <label htmlFor="cnpj" className={styles.formLabel}>
+                CNPJ
+              </label>
+              <input
+                type="text"
+                id="cnpj"
+                name="cnpj"
+                value={formData.cnpj}
+                onChange={handleChange}
+                className={styles.formInput}
+                placeholder="Digite o CNPJ"
+              />
+              {errors.cnpj && (
+                <span className={styles.error}>{errors.cnpj}</span>
+              )}
+            </div>
+            <div className={styles.formGroup}>
+              <label htmlFor="phone" className={styles.formLabel}>
+                Telefone
+              </label>
+              <input
+                type="tel"
+                id="phone"
+                name="phone"
+                value={formData.phone}
+                onChange={handleChange}
+                className={styles.formInput}
+                placeholder="Digite seu telefone"
+              />
+              {errors.phone && (
+                <span className={styles.error}>{errors.phone}</span>
+              )}
+            </div>
+            <button type="submit" className={styles.formButton}>
+              Register
+            </button>
+            <button type="button" className={styles.formButton}>
+              <Link href="/">Voltar</Link>
+            </button>
+          </form>
+        </div>
       </div>
     </main>
   );
