e9c0216a091d5a34e5b068c506461398ce371bb6
Lincon <linkon789@gmail.com>
2025-07-05 10:29:45 -0300

    feat: remove cabeçalho de políticas de segurança desnecessário

--- next.config.ts
+++ next.config.ts
@@ -43,14 +43,6 @@ const nextConfig: NextConfig = {
           { key: "Cross-Origin-Embedder-Policy", value: "require-corp" },
           { key: "Cross-Origin-Opener-Policy", value: "same-origin" },
           { key: "Cross-Origin-Resource-Policy", value: "same-origin" },
-          {
-            key: "Content-Security-Policy",
-            value:
-              "default-src 'self' https://static.cloudflareinsights.com; " +
-              "script-src 'self' https://static.cloudflareinsights.com 'unsafe-inline'; " +
-              "style-src 'self' 'unsafe-inline';",
-          },
-          { key: "Access-Control-Allow-Origin", value: "*" },
         ],
       },
 
