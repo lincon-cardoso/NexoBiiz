84265d3cec01df07e03173200cb35b076a9e2c58
Lincon <linkon789@gmail.com>
2025-07-12 15:27:16 -0300

    feat: adicionar estilos móveis para as seções de tutorial e suporte, incluindo mixins para responsividade

--- src/app/tutorial/page.tsx
+++ src/app/tutorial/page.tsx
@@ -7,49 +7,45 @@ export default function TutorialPage() {
     <div className={styles.tutorialPage}>
       <Header />
       <main className={styles.mainContent}>
-        <section className={styles.introSection}>
-          <h1 className={styles.title}>Como usar o NexoBiiz</h1>
-          <h2 className={styles.subtitle}>
-            Aprenda a utilizar o NexoBiiz para gerenciar seu negócio de forma
-            simples e eficiente.
-          </h2>
-        </section>
         <section className={styles.sectionsContainer}>
           <section className={styles.stepsSection}>
-            <div className={styles.step}>
-              <h1 className={styles.stepTitle}>Passo a Passo</h1>
-              <div className={styles.stepsContainer}>
-                <p className={styles.stepDescription}>
-                  <span className={styles.span}>1. Cadastro de Produtos: </span>{" "}
-                  Adicione os produtos que você vende, com nome, preço e
-                  quantidade. e selecione a categoria
-                </p>
-                <p className={styles.stepDescription}>
-                  <span className={styles.span}>2. Registro de Vendas:</span>{" "}
-                  Registre cada venda realizada para manter o controle
-                  financeiro.
-                </p>
-                <p className={styles.stepDescription}>
-                  <span className={styles.span}> 3. Relatórios: </span> Acesse
-                  relatórios detalhados para acompanhar o desempenho do seu
-                  negócio.
-                </p>
-              </div>
+            <h2 className={styles.stepTitle}>Passo a Passo</h2>
+            <div className={styles.stepsContainer}>
+              <p className={styles.stepDescription}>
+                <span className={styles.span}>1. Cadastro de Produtos: </span>
+                Adicione os produtos que você vende, com nome, preço e
+                quantidade. e selecione a categoria
+              </p>
+              <p className={styles.stepDescription}>
+                <span className={styles.span}>2. Registro de Vendas:</span>
+                Registre cada venda realizada para manter o controle financeiro.
+              </p>
+              <p className={styles.stepDescription}>
+                <span className={styles.span}>3. Relatórios: </span>
+                Acesse relatórios detalhados para acompanhar o desempenho do seu
+                negócio.
+              </p>
             </div>
           </section>
           <section className={styles.tipsSection}>
-            <h1 className={styles.tipsTitle}>Dicas Úteis</h1>
-            <p className={styles.tipsDescription}>
-              - Mantenha seus produtos sempre atualizados para evitar erros nas
-              vendas.
-              <br />
-              - Utilize os relatórios para identificar quais produtos estão
-              vendendo mais.
-              <br />
-              - Faça backup dos seus dados regularmente para evitar perdas.
-              <br />- Explore todas as funcionalidades do NexoBiiz para otimizar
-              a gestão do seu negócio.
-            </p>
+            <h2 className={styles.tipsTitle}>Dicas Úteis</h2>
+            <ul className={styles.tipsList}>
+              <li className={styles.tipItem}>
+                Mantenha seus produtos sempre atualizados para evitar erros nas
+                vendas.
+              </li>
+              <li className={styles.tipItem}>
+                Utilize os relatórios para identificar quais produtos estão
+                vendendo mais.
+              </li>
+              <li className={styles.tipItem}>
+                Faça backup dos seus dados regularmente para evitar perdas.
+              </li>
+              <li className={styles.tipItem}>
+                Explore todas as funcionalidades do NexoBiiz para otimizar a
+                gestão do seu negócio.
+              </li>
+            </ul>
           </section>
         </section>
       </main>
