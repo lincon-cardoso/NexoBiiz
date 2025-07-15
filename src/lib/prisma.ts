09f49c3957b3a935d0048765036db7881329a7a6
Lincon <linkon789@gmail.com>
2025-07-14 23:02:35 -0300

    fix: ajustar declaração do prisma para permitir valor indefinido e melhorar o log

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,10 +1,15 @@
 import { PrismaClient } from "@prisma/client";
 
-// Singleton pattern para evitar múltiplas instâncias no serverless
 declare global {
+  // allow global `var` declarations
   // eslint-disable-next-line no-var
-  var prisma: PrismaClient;
+  var prisma: PrismaClient | undefined;
 }
 
-export const prisma: PrismaClient = global.prisma || new PrismaClient();
+export const prisma =
+  global.prisma ||
+  new PrismaClient({
+    log: ["query"],
+  });
+
 if (process.env.NODE_ENV !== "production") global.prisma = prisma;
