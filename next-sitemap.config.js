4c742c87bdcc991e55922a7d7d29d7452a1a2213
Lincon <linkon789@gmail.com>
2025-07-15 19:59:27 -0300

    feat: adicionar configurações de sitemap, incluindo tamanho, frequência de alteração e prioridade

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,4 +2,7 @@
 module.exports = {
   siteUrl: "https://nexobiiz.devlincon.com.br", // Substitua pelo URL do seu site
   generateRobotsTxt: true, // Gera o arquivo robots.txt
+  sitemapSize: 7000,
+  changefreq: "monthly",
+  priority: 0.7,
 };
