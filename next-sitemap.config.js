d1602dce273ec6dd3ac0ee547042e5f7a51d847e
Lincon <linkon789@gmail.com>
2025-07-15 00:06:51 -0300

    feat: adicionar configuração inicial do next-sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://nexobiiz.devlincon.com.br", // Substitua pelo URL do seu site
+  generateRobotsTxt: true, // Gera o arquivo robots.txt
+};
